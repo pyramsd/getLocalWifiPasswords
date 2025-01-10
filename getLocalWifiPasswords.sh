c465529e12fc9fc200b26071e4298ff895b7e638
pyramsd <minauzum56@gmail.com>
2025-01-10 15:21:32 -0500

    update getLocalWifiPasswords

--- getLocalWifiPasswords.sh
+++ getLocalWifiPasswords.sh
@@ -5,14 +5,16 @@ if [ $EUID -ne 0 ]; then
         exit 1
 fi
 
+output="SSID\tPSK"
+
 for file in /etc/NetworkManager/system-connections/*; do
     if [ -f "$file" ]; then
-        # Lee las líneas que contienen 'id' y 'psk'
         id=$(grep '^id=' "$file" | cut -d '=' -f 2 | tr -d ' ')
         psk=$(grep '^psk=' "$file" | cut -d '=' -f 2 | tr -d ' ')
-        # Si ambos valores están presentes, imprímelos
         if [ -n "$id" ] && [ -n "$psk" ]; then
-            echo "$id = $psk"
+            output="$output\n$id\t$psk"
         fi
     fi
 done
+
+echo -e "$output" | column -s $'\t' -t
